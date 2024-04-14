700c6f5c3e45c6751286ea16637eaf207337c3bd
Ajmal-AJ <ajmalaj6633@gmail.com>
2024-04-15 00:27:07 +0530

    menu creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { Login2Component } from './login2/login2.component';
+import { Layout2Component } from './layout2/layout2.component';
 
 export const routes: Routes = [
 
-    { path:'',component:Login2Component,},
-    {path:'layout',component:LayoutComponent}
+    //{ path:'',component:Login2Component,},
+    {path:'layout',component:LayoutComponent},
+    {path:'',component:Layout2Component}
 ];
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavabarComponent } from './navabar/navabar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NavabarComponent],
+  imports: [NavabarComponent,CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+ 
 
 }
