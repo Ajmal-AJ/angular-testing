850e7584992c654ee54d389a4edb4a487d61e454
Ajmal-AJ <ajmalaj6633@gmail.com>
2024-04-15 00:37:29 +0530

    component created

--- src/app/layout2/layout2.component.ts
+++ src/app/layout2/layout2.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout2',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './layout2.component.html',
   styleUrl: './layout2.component.scss'
 })
